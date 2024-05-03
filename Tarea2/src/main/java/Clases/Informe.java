8a0806cd77f123201b5e961ffd3c1e87c7fde6e4
sshiro0 <francnunez2023@inf.udec.cl>
2024-05-03 11:49:13 -0400

    Añadido método generarInforme, revisar comentarios

--- Tarea2/src/main/java/Clases/Informe.java
+++ Tarea2/src/main/java/Clases/Informe.java
@@ -1,21 +1,34 @@
 package Clases;
 import java.io.File;
-import java.util.Objects;
-
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Informe {
-    public Informe(){
-
-
-
-
+    public Informe() {
     }
-
-    public void generarInforme(){
-
-
+    public void generarInforme() {
+        try {
+            File archivo = new File("informeReunión.txt");
+            int x=1;
+            while(archivo.exists()) {
+                File archivoRenombrado = new File("informeReunión"+x+".txt");
+                archivo.renameTo(archivoRenombrado);
+                x++;
+            }
+            FileWriter informe = new FileWriter(archivo);
+            informe.write("INFORME DE LA REUNIÓN\n\n");
+            informe.write("Fecha y hora:" + "\n"); // fecha y horaPrevista
+            informe.write("Hora de inicio:" + "\n"); // horaInicio
+            informe.write("Hora de fin:" + "\n"); // horaFin
+            informe.write("Duración:" + "\n"); // duracionPrevista
+            informe.write("Tipo de reunión::" + "\n"); // tipoReunion
+            informe.write("Enlace/Sala:" + "\n"); // enlace si es ReunionVirtual, sala si es ReunionPresencial
+            informe.write("Lista de Participantes:" + "\n"); // TotalAsistencia
+            informe.write("Notas de la reunión:" + "\n"); // Nota
+            informe.close();
+        }
+        catch(IOException exception) {
+            exception.printStackTrace(System.out);
+        }
     }
-
-
-
 }
