db4208cd01e3e4d5a8a0e98a4c62413fa037bb2e
sshiro0 <francnunez2023@inf.udec.cl>
2024-05-14 16:47:06 -0400

    Añadido Javadoc de Departamento

--- Tarea2/src/main/java/Clases/Departamento.java
+++ Tarea2/src/main/java/Clases/Departamento.java
@@ -2,26 +2,54 @@ package Clases;
 
 import java.util.ArrayList;
 
+/**
+ * Departamento representa un departamento conformado por un grupo de empleados, el cual puede ser invitado a la reunión.
+ */
 public class Departamento {
     private String nombreDepartamento;
     private ArrayList<Empleado> empleados = new ArrayList<>();
 
+    /**
+     * Constructor de Departamento.
+     *
+     * @param nombre Nombra al departamento.
+     */
     public Departamento(String nombre){
         this.nombreDepartamento = nombre;
     }
 
+    /**
+     * Método que añade empleados para que sean parte del departamento.
+     *
+     * @param empleado Empleado que se añadirá al departamento.
+     */
     public void addEmpleado(Empleado empleado){
         empleados.add(empleado);
     }
 
+    /**
+     * Método que permite expulsar empleados del departamento.
+     *
+     * @param empleado Empleado a eliminar del departamento.
+     */
     public void deleteEmpleado(Empleado empleado){
         empleados.remove(empleados.indexOf(empleado));
     }
 
+    /**
+     * Método getter para obtener el ArrayList con los empleados del departamento.
+     *
+     * @return ArrayList de empleados.
+     */
     public ArrayList<Empleado> getEmpleados(){
         return empleados;
     }
 
+    /**
+     * Método getter para obtener el nombre del departamento.
+     *
+     * @return nombreDepartamento Nombre del departmento.
+     */
     public String getNombreDepartamento(){
         return nombreDepartamento;
     }
