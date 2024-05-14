ca5474dd55be1305fb283920c8d4c298cb7dbc8f
Kingsephir <feliperaysz3@gmail.com>
2024-05-14 16:13:58 -0400

    Añadido clases Reunion y Asistencia

--- Tarea2/src/main/java/Clases/Asistencia.java
+++ Tarea2/src/main/java/Clases/Asistencia.java
@@ -1,18 +1,113 @@
 package Clases;
+
+import Excepciones.NoFinalizadoReunionException;
+import Excepciones.NoIniciadoReunionException;
+import Excepciones.ReunionFinalizadaException;
+
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
-public class Asistencia {
-    ArrayList<Empleado> listaAsistencia;
-    ArrayList<Instant> listaHoraLlegada;
+/**
+ * Asistencia se encarga de gestionar la asistencia de los empleados, si están a tiempo, atrasados o no llegan a la reunión.
+ */
+public class Asistencia{
+    private final ArrayList<Empleado> listaAsistencia;
+    private final ArrayList<Empleado> listaAusencia;
+    private final ArrayList<Instant> listaHoraLlegada;
+    private final ArrayList<Empleado> listaAtraso;
 
-
-    public Asistencia(){}
-
-    public void addEmpleado(Empleado empleado){
-        listaAsistencia.add(empleado);
+    /**
+     * Constructor de Asistencia
+     * Inicializa los ArrayLists para las asistencias
+     */
+    public Asistencia() {
+        listaAsistencia = new ArrayList<>();
+        listaAusencia = new ArrayList<>();
+        listaHoraLlegada = new ArrayList<>();
+        listaAtraso = new ArrayList<>();
     }
 
+    /**
+     * Método para obtener la lista de empleados que llegan tarde a la reunión.
+     * Se considerará "tarde" 5 minutos después de la hora de inicio.
+     * @param horaInicio Instant que indica el momento en el que se inicia la reunión.
+     * @return ArrayList de empleados que llegaron tarde.
+     * @throws NoIniciadoReunionException Se lanza si la reunión no ha iniciado.
+     */
+    public ArrayList<Empleado> getListaAtraso(Instant horaInicio) throws NoIniciadoReunionException{
+        if (horaInicio == null || horaInicio.isAfter(Instant.now())){
+            throw new NoIniciadoReunionException();
+        }
+
+        Instant horaAusencia = horaInicio.plus(5, ChronoUnit.MINUTES);
+        for (int i = 0; i < listaHoraLlegada.size(); i++) {
+            if(horaAusencia.isBefore(listaHoraLlegada.get(i))){
+                listaAtraso.add(listaAsistencia.get(i));
+            }
+        }
+        return listaAtraso;
+    }
+
+    /**
+     * Método para obtener la lista de empleados que no asistieron a la reunión.
+     * Para iniciar este método la reunión debe haber finalizado.
+     * @param listaInvitados Lista de empleados invitados a la reunión.
+     * @param horaInicio Instant que indica el momento en el que se inicia la reunión.
+     * @param horaFinal Instant que indica el momento en el que se termina la reunión
+     * @return ArrayList de empleados que no llegaron.
+     * @throws NoIniciadoReunionException Se lanza si la reunión no ha iniciado.
+     * @throws NoFinalizadoReunionException Se lanza si la reunión no ha terminado.
+     */
+    public ArrayList<Empleado> getListaAusencia(ArrayList<Empleado> listaInvitados, Instant horaInicio, Instant horaFinal) throws NoIniciadoReunionException, NoFinalizadoReunionException {
+        if(horaInicio == null || horaInicio.isAfter(Instant.now())){
+            throw new NoIniciadoReunionException();
+        }
+
+        else if (horaFinal == null){
+            throw new NoFinalizadoReunionException();
+        }
+
+        for (int i = 0; i < listaInvitados.size(); i++) {
+            if (!listaAsistencia.contains(listaInvitados.get(i))){
+                listaAusencia.add(listaInvitados.get(i));
+            }
+        }
+        return listaAusencia;
+    }
+
+    /**
+     * Método para obtener la lista de empleados que asistieron a la reunión.
+     * No hace diferencia entre atrasados y puntuales.
+     * @param horaInicio Instant que indica el momento en el que se inicia la reunión.
+     * @return ArrayList de empleados que asistieron.
+     * @throws NoIniciadoReunionException Se lanza si la reunión no ha iniciado.
+     */
+    public ArrayList<Empleado> getListaAsistencia(Instant horaInicio) throws NoIniciadoReunionException{
+        if (horaInicio == null || horaInicio.isAfter(Instant.now())){
+            throw new NoIniciadoReunionException();
+        }
+
+        return listaAsistencia;
+    }
+
+    /**
+     * Método que registra la llegada de un empleado a la reunión junto a su hora.
+     * @param empleado Empleado que llega a la reunión.
+     * @param horaInicio Instant que indica el momento en el que se inicia la reunión.
+     * @param horaFinal Instant que indica el momento en el que se termina la reunión.
+     * @throws NoIniciadoReunionException Se lanza si la reunión no ha iniciado.
+     * @throws ReunionFinalizadaException Se lanza si la reunión no ha terminado.
+     */
+    public void addLlegada(Empleado empleado, Instant horaInicio, Instant horaFinal) throws NoIniciadoReunionException, ReunionFinalizadaException {
+        if (horaInicio == null || Instant.now().isBefore(horaInicio)){
+            throw new NoIniciadoReunionException();
+        }
+        else if(horaFinal != null){
+            throw new ReunionFinalizadaException();
+        }
+        listaHoraLlegada.add(Instant.now());
+        listaAsistencia.add(empleado);
+    }
+    
 }
-
-
--- Tarea2/src/main/java/Clases/Reunion.java
+++ Tarea2/src/main/java/Clases/Reunion.java
@@ -1,42 +1,272 @@
 package Clases;
 
+import Excepciones.NoFinalizadoReunionException;
+import Excepciones.NoIniciadoReunionException;
+import Excepciones.ParametrosInvalidosException;
+import Excepciones.ReunionFinalizadaException;
+
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
+import java.time.Duration;
 
-public abstract class Reunion {
-    private Date fecha;
+/**
+ * Clase abstracta que representa una Reunion
+ */
+public abstract class Reunion{
     private Instant horaPrevista;
+    private Date fecha;
     private Instant horaInicio;
     private Instant horaFinal;
-    private TipoReunion tipoReunion;
+    private TipoReunion tipo;
+    private Duration duracionPrevista;
+    private Duration duracionReunion;
+    private Invitacion invitacionReunion;
+    private ArrayList<Nota> notas;
+    private Asistencia asistencia;
 
+    /**
+     * Constructor de la clase Reunion
+     *
+     * @param invitacion Instancia de clase Invitacion que es usada para obtener informacion de los Empleados invitados
+     */
+    public Reunion(Invitacion invitacion){
+        this.invitacionReunion = invitacion;
+        this.duracionPrevista = invitacionReunion.getDuracionPrevista();
+        this.fecha= invitacionReunion.getFecha();
+        this.horaPrevista = invitacionReunion.getHoraPrevista();
+        this.tipo = invitacionReunion.getTipo();
+        notas = new ArrayList<>();
+        this.asistencia = new Asistencia();
+    }
 
-    public Reunion(){
+    /**
+     * Metodo para revisar que la instancia de Invitacion que se pasa por Reunion no sea nula
+     *
+     * @throws ParametrosInvalidosException excepcion por si invitacion es null
+     */
+    public void checkParameters() throws ParametrosInvalidosException {
+        if (invitacionReunion == null){
+            throw new ParametrosInvalidosException();
+        }
+    }
 
-    }
-    public ArrayList<Empleado> obtenerAsistencia(){
+    /**
+     * Metodo abstracto que obtiene el enlace/sala de las reuniones
+     *
+     * @return String con el enlace
+     */
+    public abstract String getAcceso();
 
-    }
-    public ArrayList<Empleado> obtenerAusencias(){
+    /**
+     * Metodo para iniciar la reunion
+     */
+    public abstract void iniciar();
 
-    }
-    public ArrayList<Empleado> obtenerRetrasos(){
+    /**
+     * Metodo para finalizar la reunion
+     */
+    public abstract void finalizar();
 
+    /**
+     * Metodo setter para la variable horaInicio
+     *
+     * @param horaInicio Hora inicial de la reunion
+     */
+    public void setHoraInicio(Instant horaInicio) {
+        this.horaInicio = horaInicio;
     }
-    public int obtenerTotalAsistencia(){
 
+    /**
+     * Metodo setter para la variable horaFinal
+     *
+     * @param horaFinal Hora final de la reunion
+     */
+    public void setHoraFinal(Instant horaFinal) {
+        this.horaFinal = horaFinal;
     }
-    public float PorcentajeAsistencia(){
-        // Tomar cantidad de invitados, cantidad de asistencias de obtTotalAsistencia, con eso calcular %
+
+    /**
+     * Metodo getter para obtener la hora de inicio de la reunion
+     *
+     * @return horaInicio de Reunion
+     * @throws NoIniciadoReunionException Excepcion si no se ha iniciado la reunion
+     */
+    public Instant getHoraInicio() throws NoIniciadoReunionException{
+        if (horaInicio == null){
+            throw new NoIniciadoReunionException();
+        }
+
+        return horaInicio;
     }
-    public float calcularTiempoReal(){
-        // concepto: Hora inicio - hora final
+
+    /**
+     * Metodo getter para obtener la hora final de la reunion
+     *
+     * @return horaFinal de Reunion
+     * @throws NoFinalizadoReunionException Excepcion si no ha finalizado la reunion
+     */
+    public Instant getHoraFinal() throws NoFinalizadoReunionException{
+        if (horaFinal == null){
+            throw new NoFinalizadoReunionException();
+        }
+
+        return horaFinal;
     }
-    public void iniciar(){
-        // Declara hora inicio
+
+    /**
+     * Metodo para crear una instancia de Nota y luego añadirla al ArrayList de notas
+     *
+     *
+     * @param contenido String que contiene la informacion de la nota
+     * @throws NoIniciadoReunionException Excepcion si no ha iniciado la reunion
+     */
+    public void createNota(String contenido) throws NoIniciadoReunionException {
+        if (horaInicio != null){
+        notas.add(new Nota(contenido));
+        }
+        else{
+            throw new NoIniciadoReunionException();
+        }
     }
-    public void finalizar(){
-        // Declara hora final
+
+    /**
+     * Metodo getter para obtener el ArrayList con las notas
+     *
+     * @return ArrayList de objeto Nota
+     */
+    public ArrayList<Nota> getNotas(){
+        return notas;
+    }
+
+    /**
+     * Metodo getter para obtener la Fecha de la reunion
+     *
+     * @return variable Fecha de tipo Date
+     */
+    public Date getFecha(){
+        return fecha;
+    }
+
+    /**
+     * Metodo getter para obtener el tipo de reunion qu ese está realizando
+     *
+     * @return variable tipo de tipo TipoReunion
+     */
+    public TipoReunion getTipo(){
+        return tipo;
+    }
+
+    /**
+     * Metodo getter para obtener la hora prevista de la reunion
+     *
+     * @return variable horaPrevista de tipo Instant
+     */
+    public Instant getHoraPrevista(){
+        return horaPrevista;
+    }
+
+    /**
+     * Metodo getter para obtener la duracion total de la reunion
+     *
+     * @return duracionReunion de tipo Duration
+     * @throws NoFinalizadoReunionException Excepcion si no ha finalizado la reunion
+     */
+    public Duration getDuracionReunion() throws NoFinalizadoReunionException{
+        if (horaInicio != null && horaFinal != null) {
+            duracionReunion = Duration.between(horaInicio, horaFinal);
+        }
+        else {
+            throw new NoFinalizadoReunionException(); // Exception
+        }
+        return duracionReunion;
+    }
+
+    /**
+     * Metodo getter para obtener la duracion prevista de la reunion
+     *
+     * @return variable duracionPrevista de tipo Duration
+     */
+    public Duration getDuracionPrevista(){
+        return duracionPrevista;
+    }
+
+    /**
+     * Metodo getter para obtener el ArrayList con los empleados que están asistiendo actualmente a la reunion
+     *
+     * @return ArrayList de Empleados
+     * @throws NoIniciadoReunionException Excepcion por si no ha iniciado la reunion
+     */
+    public ArrayList<Empleado> getAsistencias() throws NoIniciadoReunionException {
+        return asistencia.getListaAsistencia(this.horaInicio);
+    }
+
+    /**
+     * Metodo getter para obtener el ArrayList con los empleados que no asistieron a la reunion
+     *
+     * @return Arraylist de Empleados
+     * @throws NoFinalizadoReunionException Excepcion por si no ha finalizado la reunion
+     * @throws NoIniciadoReunionException Excepcion por si no ha iniciado la reunion
+     */
+    public ArrayList<Empleado> getAusencias() throws NoFinalizadoReunionException, NoIniciadoReunionException {
+        return asistencia.getListaAusencia(invitacionReunion.getListaInvitados(), this.horaInicio, this.horaFinal);
+    }
+
+    /**
+     * Metodo getter para obtener el ArrayList con los empleados que han llegado atrasados actualmente
+     *
+     * @return ArrayList de Empleados
+     * @throws NoIniciadoReunionException Excepcion por si no ha iniciado la reunion
+     */
+    public ArrayList<Empleado> getAtrasos() throws NoIniciadoReunionException {
+        return asistencia.getListaAtraso(this.horaInicio);
+    }
+
+    /**
+     * Metodo getter para obtener la cantidad de Empleados que asistieron a la reunion
+     *
+     * @return Entero con la cantidad de Empleados
+     * @throws NoIniciadoReunionException Excepcion por si no ha iniciado la reunion
+     * @throws NoFinalizadoReunionException Excepcion por si no ha finalizado la reunion
+     */
+    public int getTotalAsistencia() throws NoIniciadoReunionException, NoFinalizadoReunionException {
+        if (horaFinal == null){
+            throw new NoFinalizadoReunionException();
+        }
+        return getAsistencias().size();
+    }
+
+    /**
+     * Metodo getter para obtener el porcentaje de Empleados que asistieron a la reunion en comparacion del total invitado
+     *
+     * @return un numero flotante que representa el porcentaje
+     * @throws NoIniciadoReunionException Excepcion por si no ha iniciado la reunion
+     * @throws NoFinalizadoReunionException Excepcion por si no ha finalizado la reunion
+     */
+    public float getPorcentajeAsistencia() throws NoIniciadoReunionException, NoFinalizadoReunionException {
+        int cantidadAsistentes = getTotalAsistencia();
+        int cantidadInvitados = invitacionReunion.getListaInvitados().size();
+
+        return (100 * (float)cantidadAsistentes) / (float)cantidadInvitados;
+    }
+
+    /**
+     * Metodo getter para obtener el la duracion de la reunion en segundos
+     *
+     * @return la duracion de la reunion
+     */
+    public float getTiempoReal(){
+        return (float)duracionReunion.toSeconds();
+    }
+
+    /**
+     * Metodo para añadir empleados a la lista de asistencia
+     *
+     * @param empleado Empleado que se añade
+     * @throws ReunionFinalizadaException Excepcion por si ya finalizo la reunion
+     * @throws NoIniciadoReunionException Excepcion por si no ha iniciado la reunion
+     */
+    public void addLlegada(Empleado empleado) throws ReunionFinalizadaException, NoIniciadoReunionException {
+        asistencia.addLlegada(empleado, horaInicio, horaFinal);
     }
 }
