3e8f54710d894934d2d81d4a75e991a192fb8113
Kingsephir <feliperaysz3@gmail.com>
2024-05-14 16:25:38 -0400

    Añadido clase Nota

--- Tarea2/src/main/java/Clases/Nota.java
+++ Tarea2/src/main/java/Clases/Nota.java
@@ -1,12 +1,25 @@
 package Clases;
 
+/**
+ * Clase que representa una nota
+ */
 public class Nota {
     private String contenido;
 
+    /**
+     * Constructor de la clase
+     *
+     * @param contenido String que contiene el contenido de la nota
+     */
     public Nota(String contenido){
         this.contenido = contenido;
     }
 
+    /**
+     * Metodo getter para obtener el contenido de la nota
+     *
+     * @return variable contenido de tipo String
+     */
     public String getContenidoNota(){
         return contenido;
     }
