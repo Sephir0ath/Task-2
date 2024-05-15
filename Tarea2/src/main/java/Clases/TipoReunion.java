0bf52ccac74d29a312c5d72649731fb91e6455b8
Mazulin <matijenven2004@gmail.com>
2024-05-15 16:54:20 -0400

    Añadido Javadoc para TipoReunion

--- Tarea2/src/main/java/Clases/TipoReunion.java
+++ Tarea2/src/main/java/Clases/TipoReunion.java
@@ -1,7 +1,19 @@
 package Clases;
 
+/**
+ * Metodo enum, que representa los diferentes tipos de Reunion
+ */
 public enum TipoReunion {
+    /**
+     * Tipo de Reunion Tecnica
+     */
     TECNICA,
+    /**
+     * Tipo de Reunion Marketing
+     */
     MARKETING,
+    /**
+     * Cualquier Otro tipo de Reunion
+     */
     OTRO
 }
